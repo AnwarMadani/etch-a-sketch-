e53e820f1c37e51984496f68999741415c9067a5
Anwar Madani <15781928+AnwarMadani@users.noreply.github.com>
2024-10-19 16:49:01 +0200

    add basic logic for drawing in black

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const container = document.querySelector('.container');
+const grid = document.querySelector('.container');
 
-createGrid(16);
+setupGrid(16);
 
-function createGrid(size){
-    container.computedStyleMap.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.computedStyleMap.gridTemplateRows = `repeat(${size}, 1fr)`;
+function setupGrid(size){
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for(let i = 0; i < size * size; i++){
-        const newDiv = document.createElement('div');
-        newDiv.addEventListener("mouseover", changeColor);
-        container.appendChild(newDiv);
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener("mouseover",(e) => changeColor(e.target));
+        grid.appendChild(gridElement);
     }
+
 }
 
-function changeColor(e){
-    e.target.style["background-color"] = 'black';
+function changeColor(element){
+    element.style["background-color"] = 'black';
 }
